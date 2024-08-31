075cc01a19563a8bf4564f8edca0ff4d08737fbc
shimo0829 <161689226+shimo0829@users.noreply.github.com>
2024-08-31 17:02:25 +0800

    Update 作業提交網站.py

--- test/project/作業提交網站.py
+++ test/project/作業提交網站.py
@@ -7,12 +7,11 @@ st.sidebar.subheader("上傳檔案")
 uploaded_file = st.sidebar.file_uploader("選擇欲上傳的檔案")
 
 if uploaded_file is not None:
-
     file_contents = uploaded_file.read()
-
+    
     with open(f'uploads/{uploaded_file.name}', 'wb') as f:
         f.write(file_contents)
-
+        
     st.success("檔案上傳成功")
 
 
