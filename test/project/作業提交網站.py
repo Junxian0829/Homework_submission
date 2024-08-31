69d3067374a1a291dd9e9e7fd0c47aacb861d554
shimo0829 <161689226+shimo0829@users.noreply.github.com>
2024-08-31 17:06:54 +0800

    Update 作業提交網站.py

--- test/project/作業提交網站.py
+++ test/project/作業提交網站.py
@@ -1,3 +1,4 @@
+import os
 import streamlit as st
 
 st.sidebar.title("作業提交網站")
@@ -7,12 +8,13 @@ st.sidebar.subheader("上傳檔案")
 uploaded_file = st.sidebar.file_uploader("選擇欲上傳的檔案")
 
 if uploaded_file is not None:
-    file_contents = uploaded_file.read()
+    file_path = os.path.join(UPLOAD_FOLDER, uploaded_file.name)
     
-    with open(f'uploads/{uploaded_file.name}', 'wb') as f:
-        f.write(file_contents)
-        
-    st.success("檔案上傳成功")
+    with open(file_path, 'wb') as f:
+        f.write(uploaded_file.read())
+    
+    st.sidebar.success("檔案上傳成功")
+    st.write(f"文件保存路径: {file_path}")
 
 
 
