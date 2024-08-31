b41bf216d9c7382c3f1af72dc68c46fe7a34833b
shimo0829 <161689226+shimo0829@users.noreply.github.com>
2024-08-31 18:33:09 +0800

    Update 作業提交網站.py

--- test/project/作業提交網站.py
+++ test/project/作業提交網站.py
@@ -1,9 +1,7 @@
-import os
 import streamlit as st
+import requests
 
-UPLOAD_FOLDER = 'uploads'
-if not os.path.exists(UPLOAD_FOLDER):
-    os.makedirs(UPLOAD_FOLDER)
+FLASK_URL = 'http://127.0.0.1:5000'
 
 st.sidebar.title("作業提交網站")
 
@@ -12,12 +10,29 @@ st.sidebar.subheader("上傳檔案")
 uploaded_file = st.sidebar.file_uploader("選擇欲上傳的檔案")
 
 if uploaded_file is not None:
-    file_path = os.path.join(UPLOAD_FOLDER, uploaded_file.name)
+     
+    file_name = uploaded_file.name
+    file_type = uploaded_file.type
+
+    if file_name.endswith('.py'):
+
+        file_content = uploaded_file.read().decode('utf-8')
+        st.write("**檔案內容預覽：**")
+        st.code(file_content, language='python')
     
-    with open(file_path, 'wb') as f:
-        f.write(uploaded_file.read())
+    else:
+        st.warning("請上傳 Python 檔案（.py）以查看預覽")
+
+    files = {'file': (uploaded_file.name, uploaded_file, uploaded_file.type)}
+    response = requests.post(f"{FLASK_URL}/upload", files=files)
     
-    st.sidebar.success("檔案上傳成功")
+    if response.status_code == 200:
+        st.sidebar.success("檔案上傳成功")
+    else:
+        st.sidebar.error("檔案上傳失敗")
+
+
+
 
 
 
