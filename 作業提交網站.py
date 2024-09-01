fd9d26fef322b7b88ae920375965b59ea9edfff7
shimo0829 <161689226+shimo0829@users.noreply.github.com>
2024-09-01 11:16:27 +0800

    Update 作業提交網站.py

--- 作業提交網站.py
+++ 作業提交網站.py
@@ -28,14 +28,11 @@ if uploaded_file is not None:
     
     else:
         st.warning("請上傳 Python 檔案（.py）以查看預覽")
-
-    status_text = st.sidebar.text("正在上傳檔案...")
-
+        
+with st.spinner("正在上傳檔案..."):
     with concurrent.futures.ThreadPoolExecutor() as executor:
         future = executor.submit(upload_to_flask, uploaded_file)
         success = future.result()
-
-        status_text.empty()
         
         if success:
             st.sidebar.success("檔案上傳成功")
